04d2cf0c781b35882918d87f4468707be3363aa9
Krzysztof Glinka <krzysztof.m.glinka@gmail.com>
2017-04-28 15:44:08 +0100

    sounds

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   function playSound(e) {
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]');
+    const audio = document.querySelector("audio[data-key="${e.keyCode}"]");
     console.log(audio);
     if (!audio) return;
     audio.currentTime = 0;
@@ -13,8 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(e.propertyName);
     this.classList.remove('playing');
   }
-  
+
   const keys = document.querySelectorAll('.key');
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
+  window.addEventListener('click', playSound);
 })
